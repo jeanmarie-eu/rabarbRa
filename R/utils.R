145efa1fedb0ea18cf06a589e5c616bd276270bd
jeanmarielepioufle <jm@nexmodeling.com>
2017-05-02 14:42:13 +0200

    update utils

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,13 @@
 #' @export
 print.rabarbRa <- function(x,...){
-  x$summary()
+  str(x$summary())
 }
+
+#' @importFrom lazyeval f_eval
+lazyeval::f_eval
+
+#' @importFrom lazyeval uq
+lazyeval::uq
+
+#' @importFrom lazyeval as.lazy
+lazyeval::as.lazy
