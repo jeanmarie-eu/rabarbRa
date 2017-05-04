685171b1ba58fbb66a610286b3ab61fd3f89f9ad
jeanmarielepioufle <jm@nexmodeling.com>
2017-05-04 15:21:51 +0200

    make values, delete and modify according to select

--- R/value.R
+++ R/value.R
@@ -1,12 +1,12 @@
 
-value <- function(df,indice=NULL,field=NULL){
-  if ((!is.null(indice)) && (!is.null(field))) {
-     return(df[indice,field])
-  } else if ((is.null(indice)) && (!is.null(field))) {
-     return(df[,field])
-  } else if ((!is.null(indice)) && (is.null(field))) {
-    return(df[indice,])
-  } else if ((is.null(indice)) && (is.null(field))) {
-    stop("function need arguments.")
+value_ <- function(df,i=NULL,j=NULL){
+  if ((!is.null(i)) && (!is.null(j))) {
+     return(df[i,j])
+  } else if ((is.null(i)) && (!is.null(j))) {
+     return(df[,j])
+  } else if ((!is.null(i)) && (is.null(j))) {
+    return(df[i,])
+  } else if ((is.null(i)) && (is.null(j))) {
+    return(df)
   }
 }
