8e988074f9c5f24f32370c6b8cc9b7ae88500987
jeanmarielepioufle <jm@nexmodeling.com>
2017-05-04 15:20:35 +0200

    merge logica and select files

--- R/select.R
+++ R/select.R
@@ -3,18 +3,33 @@ select_ <- function(df,query){
   stopifnot(inherits(query,"queries"))
   if(length(query$q)==1){
     tmp <- lapply(query$q,select_one,df=df)
-    res <- which(tmp[[1]])
+    i <- which(tmp[[1]])
     rm(tmp)
   } else if(length(query$q)>1){
     tmp <- lapply(query$q,select_one,df=df)
-    res <- which(do.call(query$logic,args=list(tmp)))
+    i <- which(do.call(query$logic,args=list(tmp)))
     rm(tmp)
   } else stop("query object required")
-  return(res)
+  return(i)
 }
 
 select_one <-function(req,df){
-  FUN <- get(req$fun_sign)
-  res <- FUN(df[[req$field]],req$value)
+  FUN <- get(q_fun_sign(req))
+  res <- FUN(df[[q_field(req)]],q_value(req))
   return(res)
+}
+
+AND <- function(...){
+  return(elogic("&&",...))
+}
+
+OR <- function(...){
+  return(elogic("||",...))
+}
+
+elogic <- function(logic,...) {
+  FUN <- function(...){
+     return(mapply(logic, ...))
+  }
+  return(do.call("FUN",args=...))
 }
